fd444b1efd95dcd7a56f9eb7aa72cb4f46a6d0cf
w0430903 <w0430903@nscc.ca>
2024-01-11 12:50:49 -0400

    completed stringstream example

--- stringstream/main.cpp
+++ stringstream/main.cpp
@@ -20,15 +20,33 @@ void command_input(){
 
     if (letter.length()>1) {
         // it must be text
+        std::cout << "You entered some text." << std::endl;
     } else {
         // possible 'I', 'L', 'E', 'D', or 'Q' commands
+        std::cout << "I think you are a command" << std::endl;
+
         // extract number
         ss >> first_number;
         if (!ss.fail()) {
+            std::cout << "You have a starting number!" << std::endl;
+
             // extract second number
             ss >> second_number;
             if (!ss.fail()) {
                 // must be a command
+                std::cout << "You have an ending number!" << std::endl;
+                std::cout << "I'll auusme you are a valid command." << std::endl;
+            } else {
+                std::cout << "You didn't have an ending number." << std::endl;
+                std::cout << "I'll still assume you are a valid command." << std::endl;
+            }
+        } else {
+            // didn't get a number, but could be a command
+            // if the entire input was only one character
+            if (user_input.length() == 1) {
+                std::cout << "I'll assume you are a command!" << std::endl;
+            } else {
+                std::cout << "You must be text instead." << std::endl;
             }
         }
     }
